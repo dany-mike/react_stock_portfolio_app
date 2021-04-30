786ec355cb29e735f9658ba76d758cfee7ee73c6
Dany <danymike.joseph@gmail.com>
2021-04-30 19:41:19 +0200

    Add page not found and switch in router

--- src/App.js
+++ src/App.js
@@ -5,29 +5,22 @@ import EditWalletPage from './pages/EditWallet/EditWalletPage';
 import SignInPage from './pages/SignIn/SignInPage'
 import SignUpPage from './pages/SignUp/SignUpPage'
 import {HomePage} from './pages/Home/HomePage'
-import IsAuth from './helpers/context/IsAuth/IsAuth';
-import {PrivateRoute} from './helpers/PrivateRoute/PrivateRoute'
 import NotFound from './components/NotFound/NotFound'
-import {useEffect, useState} from 'react'
 
-import React, { Component } from 'react'
-import { ThumbUpSharp } from '@material-ui/icons';
+export default function App() {
 
-export default class App extends Component {
-
-  render() {
-    return (
-      <Router>
+  return (
+    <Router>
       <Header/>
       <Switch>
         <Route exact path="/">
           <Redirect to="/signin" />
         </Route>
         <Route path='/wallets/:username' exact component={HomePage}/>
-        {/* <PrivateRoute exact path='/add-wallet/:username' authenticated={true} component={AddWalletPage}/> */}
+        <Route exact path='/add-wallet/:username' component={AddWalletPage}/>
         <Route exact path='/edit-wallet/:username/:walletId' component={EditWalletPage}/>
         {/* <Route path='/testpage' exact component={IsAuth}/> */}
-        <PrivateRoute exact path='/wallets/:username' component={HomePage} />
+        <Route exact path='/wallets/:username' component={HomePage} />
         <Route path='/signin' component={SignInPage}/>
         <Route path='/signup' component={SignUpPage}/>
         <Route>
@@ -35,7 +28,7 @@ export default class App extends Component {
         </Route>
       </Switch>
     </Router>
-    )
-  }
+  )
 }
+
 

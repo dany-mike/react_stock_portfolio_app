6da32c8edc43ca174f117abd307cea1ba50b0f49
Dany <danymike.joseph@gmail.com>
2021-05-01 18:13:18 +0200

    Get Wallet content data in walletPage.js with request api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import {BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
+
 import Header from './components/Header/Header'
 import AddWalletPage from './pages/AddWallet/AddWalletPage';
 import EditWalletPage from './pages/EditWallet/EditWalletPage';
+import WalletPage from './pages/Wallet/WalletPage'
 import SignInPage from './pages/SignIn/SignInPage'
 import SignUpPage from './pages/SignUp/SignUpPage'
 import {HomePage} from './pages/Home/HomePage'
 import NotFound from './components/NotFound/NotFound'
+
 import {useEffect, useState} from 'react'
 import {isUser} from './services/authenticationService'
 
@@ -15,7 +18,6 @@ export default function App() {
 
   useEffect(() => {
     (async () => {
-      console.log('test')
       const data = await isUser()
       setUser(data.data)
     })()
@@ -29,9 +31,9 @@ export default function App() {
           <Redirect to="/signin" />
         </Route>
         <Route path='/wallets/:username' exact component={HomePage}/>
+        <Route path='/wallet/:username/:walletId' exact component={WalletPage}/>
         <Route exact path='/add-wallet/:username' component={AddWalletPage}/>
         <Route exact path='/edit-wallet/:username/:walletId' component={EditWalletPage}/>
-        {/* <Route path='/testpage' exact component={IsAuth}/> */}
         <Route exact path='/wallets/:username' component={HomePage} />
         <Route path='/signin' component={SignInPage}/>
         <Route path='/signup' component={SignUpPage}/>
---
+++ src/pages/Wallet/WalletPage.js
@@ -0,0 +1,75 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { Container } from '@material-ui/core';
+import styles from './WalletPage.module.css'
+import { WalletContentByWalletId } from '../../services/walletService'
+import {useEffect, useState} from 'react'
+import { useParams, useHistory } from 'react-router-dom'
+
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+export default function WalletPage() {
+
+    const [walletContent, setWalletContent] = useState([])
+
+    const history = useHistory()
+    const { username } = useParams()
+    const { walletId } = useParams()
+
+    useEffect(() => {
+        (async () => {
+            try {
+                setWalletContent(await WalletContentByWalletId(username, walletId))
+            } catch(err) {
+                history.push('/signin')
+            }
+        })()
+    }, [username, walletId, history])
+
+    const classes = useStyles();
+    console.log(walletContent)
+    return (
+        <Container className={styles.marginTop}>
+            <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                    <TableCell>Company</TableCell>
+                    <TableCell>Symbol</TableCell>
+                    <TableCell>Stock Price(now)</TableCell>
+                    <TableCell>Stock Price(forecast)</TableCell>
+                    <TableCell>Shares Number</TableCell>
+                    <TableCell>Activity Area</TableCell>
+                    <TableCell>About</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {/* {rows.map((row) => (
+                    <TableRow key={row.name}>
+                        <TableCell component="th" scope="row">
+                        {row.name}
+                        </TableCell>
+                        <TableCell align="right">{row.calories}</TableCell>
+                        <TableCell align="right">{row.fat}</TableCell>
+                        <TableCell align="right">{row.carbs}</TableCell>
+                        <TableCell align="right">{row.protein}</TableCell>
+                    </TableRow>
+                    ))} */}
+                </TableBody>
+                </Table>
+            </TableContainer>
+        </Container>
+
+    )
+}

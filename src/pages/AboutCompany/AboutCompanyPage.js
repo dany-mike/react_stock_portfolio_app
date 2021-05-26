3c52a07673b5d475b786012915e1afa5f16ba7a3
Dany <danymike.joseph@gmail.com>
2021-05-26 18:18:28 +0200

    Add chart price in about company page

--- src/pages/AboutCompany/AboutCompanyPage.js
+++ src/pages/AboutCompany/AboutCompanyPage.js
@@ -9,7 +9,9 @@ import styles from "./AboutCompanyPage.module.css";
 import { useParams, useHistory } from "react-router-dom";
 import { getCompanyBySymbol } from "../../services/stockService";
 import { useState, useEffect } from "react";
+import {getStockPricesBySymbol, getStockNameBySymbol} from "../../services/stockService"
 import Circular from "../../components/Circular/Circular";
+import CompanyDescriptionCard from "../../components/CompanyDescriptionCard/CompanyDescriptionCard";
 
 const useStyles = makeStyles({
   root: {
@@ -32,6 +34,8 @@ export default function AboutCompanyPage() {
   const classes = useStyles();
 
   const [company, setCompany] = useState([]);
+  const [prices, setPrices] = useState([]);
+  const [profil, setProfil] = useState([]);
   const [loading, setLoading] = useState(true);
 
   const history = useHistory();
@@ -43,27 +47,33 @@ export default function AboutCompanyPage() {
     (async () => {
       try {
         setCompany(await getCompanyBySymbol(username, walletId, symbol));
-        setLoading(false)
+        const prices = await getStockPricesBySymbol(symbol);
+        const stockName = await getStockNameBySymbol(symbol);
+        setProfil(stockName);
+        setPrices(prices.reverse());
+        setLoading(false);
       } catch (err) {
-        history.push('/signin')
+        history.push("/signin");
       }
     })();
   }, [username, walletId, history, symbol]);
 
   const goBack = () => {
-    history.goBack()
-  }
+    history.goBack();
+  };
 
-    let spinner
-    let content 
+  let spinner;
+  let content;
 
-    if(loading === true) {
-        spinner = <Circular/>
-    } else {
-        content = <div><Typography variant="h3">{company.companyName}</Typography>
+  if (loading === true) {
+    spinner = <Circular />;
+  } else {
+    content = (
+      <div>
+        <Typography variant="h3">{company.companyName}</Typography>
         <br />
         <Grid container spacing={3}>
-          <Grid item xs={8}>
+          <Grid item xs={12}>
             <Card variant="outlined">
               <CardContent>
                 <Typography
@@ -79,7 +89,7 @@ export default function AboutCompanyPage() {
               </CardContent>
             </Card>
           </Grid>
-          <Grid item xs={4}>
+          <Grid item xs={12}>
             <Card variant="outlined">
               <CardContent>
                 <Typography
@@ -92,34 +102,30 @@ export default function AboutCompanyPage() {
                 <Typography variant="h4" component="h2">
                   ${company.sharesNumber * company.stockPrice}
                 </Typography>
-                <hr />
-                <Grid container spacing={3}>
-                  <Grid item xs={6}>
-                    <Typography variant="h5">$1</Typography>
-                    <Typography color="textSecondary" component="p">
-                      Invested
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography variant="h5">$1</Typography>
-                    <Typography color="textSecondary" component="p">
-                      G/L
-                    </Typography>
-                  </Grid>
-                </Grid>
               </CardContent>
             </Card>
           </Grid>
-        </Grid></div>
-    }
+          <Grid item xs={12}>
+            <CompanyDescriptionCard data={prices} profil={profil}/>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 
   return (
     <Container className={styles.marginTop}>
       {content}
       {spinner}
-        <Button color="secondary" variant="outlined" onClick={goBack} className={styles.mt} style={{margin: 2}}>
-          {"Back"}{" "}
-        </Button>
+      <Button
+        color="secondary"
+        variant="outlined"
+        onClick={goBack}
+        className={styles.mt}
+        style={{ margin: 2 }}
+      >
+        {"Back"}{" "}
+      </Button>
     </Container>
   );
 }

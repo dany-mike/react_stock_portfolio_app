ba93d1615b79e72c7fe216b6649bc5f6bbae6942
Dany <danymike.joseph@gmail.com>
2021-05-31 17:01:33 +0200

    update createWalletPage and EditWalletPage by changing baseURL

--- src/pages/AddWallet/AddWalletPage.js
+++ src/pages/AddWallet/AddWalletPage.js
@@ -4,7 +4,7 @@ import Container from '@material-ui/core/Container';
 export default function AddWalletPage() {
     return (
         <Container>
-            <WalletForm url="http://localhost:3000/wallet/add-wallet"/>
+            <WalletForm url="https://stock-portfolio-app-api.herokuapp.com/wallet/add-wallet"/>
         </Container>
     )
 }
--- src/pages/EditWallet/EditWalletPage.js
+++ src/pages/EditWallet/EditWalletPage.js
@@ -4,7 +4,7 @@ import Container from '@material-ui/core/Container';
 export default function EditWalletPage() {
     return (
         <Container>
-            <WalletForm url="http://localhost:3000/wallet/edit-wallet"/>
+            <WalletForm url="https://stock-portfolio-app-api.herokuapp.com/wallet/edit-wallet"/>
         </Container>
     )
 }
